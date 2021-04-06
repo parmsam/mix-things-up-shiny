9ff56c6581f3de7c0d390fe8f2d33529ac07c284
Sam <40672605+parmsam@users.noreply.github.com>
2021-04-06 19:59:20 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -10,44 +10,66 @@ library(downloadthis)
 library(webshot)
 # library(shinythemes)
 library(bslib)
+library(shinyjs)
 
 # get data ----
 gdocs_url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vQliF1fPTXNk6b4cVwbkD7GmYFmyNKkG2GrzYcr21d-C02L61gZZNrk3beBEf95mQ-doWd3MweAyZKH/pub?gid=0&single=true&output=csv"
 #read data from url
 exercise_data <- read_csv( url(gdocs_url) )
 
+#set dynamic themes
+light <- bslib::bs_theme(version = 4, bootswatch = "minty")
+
 # define UI ----
 ui <- fluidPage(
   # theme = shinytheme("lumen"),
-  theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
-  h4("a simple workout generator"),
+  theme = light,
+  h4("a simple workout plan generator"),
   titlePanel("Muscle Confusion"),
                 # Layout a sidebar and main area
                 sidebarLayout(
                   #side bar here ----
                   sidebarPanel(
+                    useShinyjs(),
                     
                     #define input sliders ----
                     sliderInput("n_exercises", "Step 1: Number of Exercises", value=5, min=1, max=10, 1),
-                    sliderInput("n_sets", "Step 2: Number of Sets", 1, 5, 1),
-                    sliderInput("n_reps", "Step 3: Number of Reps", 5, 20, 5),
-                    sliderInput("n_time", "Step 4: Number of Minutes", 2, 10, 2),
+                    sliderInput("n_sets", "Step 2: Number of Sets", value = 1, min=1, max=5, 1),
+                    sliderInput("n_reps", "Step 3a: Number of Reps", value = 5, min=5, max=20, 5),
+                    sliderInput("n_time", "Step 3b: Number of Minutes", valu = 2, min=1, max=10, 1),
                     #action button to run setup ----
-                    actionButton("goButton", "Get after it!", btn_type = "button", class = "btn-warning"),
+                    actionButton("goButton", "Get after it!", btn_type = "button", class = "btn-secondnary"),
                     ),
                   # main area here ----
                   mainPanel(
-                    gt_output(outputId = "gt_table")
+                    br(),
+                    gt_output(outputId = "gt_table"),
+                    br(),
+                    column(12, downloadButton(outputId = "downloadXLSX", label = "Download XLSX"), align='center'),
+                    br(),
+                    column(12, downloadButton(outputId = "downloadPNG", label = "Download PNG"), align='center')
                   )
                 )
 )
 
-server <- function(input,output) {
+server <- function(input,output, session) {
   
   # define set_or_sets variable
   set_or_sets <- reactive({ 
     if(input$n_sets > 1){"sets"}
     else {"set"}
+  })
+  
+  observe({
+    shinyjs::hide("downloadXLSX")
+    if(input$goButton)
+      shinyjs::show("downloadXLSX")
+  })
+  
+  observe({
+    shinyjs::hide("downloadPNG")
+    if(input$goButton)
+      shinyjs::show("downloadPNG")
   })
   
   data_extr <- eventReactive(input$goButton, { 
@@ -66,45 +88,41 @@ server <- function(input,output) {
   
   gt_table <- reactive({
     data_extr() %>% 
-    gt()
+    gt() %>%
+      tab_header(
+        title = md("Simple workout plan"),
+        subtitle = md("what are you waiting for?")
+      ) %>%
+      tab_source_note(
+        source_note = md("Click `Get  after it!` if changing <br>parameters. Made by **@parmsam**")
+      ) %>%
+      tab_source_note(
+        source_note = md("Consult health professional before <br>starting a fitness program")
+      )
   })
   
   output$gt_table <-
     render_gt(
-      expr = gt_table(),
-      height = px(600),
-      width = px(600)
+      expr = gt_table()
     )
   
-  # # provide download button functionality ----
-  # gtsave(gt_table,
-  #        "images", expand = 50,
-  #        file =  "temp.png")
-  # 
-  # # download button for table to xlsx 
-  # renderUI({
-  #   data_extr %>%
-  #     download_this(
-  #       output_name = str_c("rand_workout_plan_", today()),
-  #       output_extension = ".xlsx",
-  #       button_label = "Download XLSX",
-  #       button_type = "primary",
-  #     )
-  # })
-  # 
-  # # download button for table to png
-  # renderUI({
-  #   download_file(
-  #     path = "images/temp.png",
-  #     output_name = str_c("rand_workout_plan_", today()),
-  #     output_extension = ".png",
-  #     button_label = "Download PNG",
-  #     button_type = "success",
-  #     has_icon = TRUE,
-  #     icon = "fa fa-save",
-  #     self_contained = FALSE
-  #   )
-  # })
+  output$downloadXLSX <- downloadHandler(
+    filename = function() {
+      str_c('rand_workout_plan_', Sys.Date(), '.xlsx', sep='')
+      },
+    content = function(con) {
+      writexl::write_xlsx(data_extr(), con)
+      }
+    )
+  
+  output$downloadPNG <- downloadHandler(
+    filename = function() { 
+      str_c('rand_workout_plan_', Sys.Date(), '.png', sep='')
+      },
+    content = function(con) {
+      gtsave(gt_table(), expand = 50, filename =  con)
+    }
+  )
   
 }
 
